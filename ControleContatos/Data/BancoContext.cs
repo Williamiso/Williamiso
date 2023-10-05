4152ec51ed620452d2382b507aea050e35b770e8
Williamiso <william.iso@hotmail.com>
2023-10-05 12:43:31 -0300

    ControleContato

---
+++ ControleContatos/Data/BancoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ControleContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleContatos.Data
+{
+	public class BancoContext : DbContext
+	{
+		public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<ContatoModel> Contatos { get; set; }
+	}
+}
+
---
+++ ControleContatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleContatos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
